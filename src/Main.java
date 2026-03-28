172c737650c533d91b173b431da6995084916a37
KU12 <KU12@SALAA0607.salasuts.uts.edu.co>
2026-03-27 19:27:09 -0500

    agregamos clase backend y package

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author KU12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
