643f4e0954d02cf66386ba6b42c0ee6f1674dc04
KU12 <KU12@SALAA0601.salasuts.uts.edu.co>
2026-03-27 19:27:06 -0500

    se agrega clase y package

---
+++ src/primertaller/flores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primertaller;
+
+/**
+ *
+ * @author KU12
+ */
+public class flores {
+    private String tamaño;
+    
+    public static void main (String[] args){
+        System.out.println("buenas buenas");
+    
+    }
+    
+  
+}
