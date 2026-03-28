f6fbfdecc0b4b78ab2c7c55edbb522c944150d3c
KU12 <KU12@SALAA0603.salasuts.uts.edu.co>
2026-03-27 19:26:56 -0500

    Se agrega clase y package

---
+++ src/SegurityAlbert/Perro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SegurityAlbert;
+
+/**
+ *
+ * @author KU12
+ */
+public class Perro {
+
+        public static void main(String[] args) {
+            
+
+        // TODO code application logic here
+        
+            System.out.println("Hola mundo");
+    }
+}
